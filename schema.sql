1fc41d150fdcb2c6b111e7271933bca04fd0f15e
Li Shuotong <Li@Shuotongs-MacBook.local>
2021-11-16 18:44:31 -0500

    schema.sql: added insertion code from table owid_corona_data to specific table defined in schema

--- schema.sql
+++ schema.sql
@@ -15,31 +15,31 @@ SET SEARCH_PATH TO COVID19;
 CREATE TABLE Continent (
 	continentName TEXT,
 	countryName TEXT NOT NULL UNIQUE, --Unique Needed for setting foreign key
-	PRIMARY KEY (continentName)
+	PRIMARY KEY (continentName, countryName)
 );
 
 --Complete
 CREATE TABLE Country (
-	iso_code VARCHAR(3), --3 length fixed string?
+	iso_code VARCHAR(8), 
 	countryName TEXT NOT NULL UNIQUE, --The ISO CODE is sufficient as a primary key
-	population INT,
+	population BIGINT, --certain value in CSV (ex. World) is greater than the range of int
 	gdp_per_capita FLOAT,
 	PRIMARY KEY(iso_code),
 	FOREIGN KEY (countryName) REFERENCES Continent(countryName)
-);
+);C 
 
 --Complete
 --As many data may be missing, the NOT NULL constrain is not used here
 CREATE TABLE CoronaData (
-	iso_code VARCHAR(3),
+	iso_code VARCHAR(8),
 	d TIMESTAMP, --cannot use data as this is a keyword in sql
-	total_cases INT,
-	total_deaths INT,
-	reproduction_rate INT,
-	total_tests INT ,
-	total_vaccination INT,
-	peope_vaccinated INT ,
-	people_fully_vaccinated INT ,
+	total_cases BIGINT,
+	total_deaths BIGINT,
+	reproduction_rate FLOAT,
+	total_tests BIGINT ,
+	total_vaccinations BIGINT,
+	people_vaccinated BIGINT ,
+	people_fully_vaccinated BIGINT ,
 	stringency_index FLOAT ,
 	PRIMARY KEY (iso_code,d),
 	FOREIGN KEY (iso_code) REFERENCES Country(iso_code)
@@ -48,7 +48,7 @@ CREATE TABLE CoronaData (
 --Complete
 --Variable type is checked
 CREATE TABLE MedicalInfo (
-	iso_code VARCHAR(3),
+	iso_code VARCHAR(8),
 	cardiovasc_death_rate FLOAT,
 	diabetes_prevalence FLOAT,
 	hospital_beds_per_thousand FLOAT,
@@ -63,7 +63,7 @@ CREATE TABLE MedicalInfo (
 --Complete
 --Variable type is checked
 CREATE TABLE DemographicInfo (
-	iso_code VARCHAR(3),
+	iso_code VARCHAR(8),
 	population_density FLOAT,
 	median_age FLOAT,
 	aged_65_old FLOAT,
@@ -73,20 +73,7 @@ CREATE TABLE DemographicInfo (
 	PRIMARY KEY (iso_code),
 	FOREIGN KEY (iso_code) REFERENCES Country(iso_code)
 );
-Drop Table if EXISTS Persons;
-CREATE TABLE persons (
-  id SERIAL,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  dob TIMESTAMP,
-  email VARCHAR(255),
-  EID float--need to find a way to convert float to int
-);
---important: copy is not a SQL command and must cp in single line to the command window
-\COPY persons (first_name, last_name, dob, email,eid)from 'persons.csv'DELIMITER ',' CSV HEADER
 
--- For the reason above, do not change this line
-\COPY owid_covid_data (iso_code,continent,location,d,total_cases,new_cases,new_cases_smoothed,total_deaths,new_deaths,new_deaths_smoothed,total_cases_per_million,new_cases_per_million,new_cases_smoothed_per_million,total_deaths_per_million,new_deaths_per_million,new_deaths_smoothed_per_million,reproduction_rate,icu_patients,icu_patients_per_million,hosp_patients,hosp_patients_per_million,weekly_icu_admissions,weekly_icu_admissions_per_million,weekly_hosp_admissions,weekly_hosp_admissions_per_million,new_tests,total_tests,total_tests_per_thousand,new_tests_per_thousand,new_tests_smoothed,new_tests_smoothed_per_thousand,positive_rate,tests_per_case,tests_units,total_vaccinations,people_vaccinated,people_fully_vaccinated,total_boosters,new_vaccinations,new_vaccinations_smoothed,total_vaccinations_per_hundred,people_vaccinated_per_hundred,people_fully_vaccinated_per_hundred,total_boosters_per_hundred,new_vaccinations_smoothed_per_million,stringency_index,population,population_density,median_age,aged_65_older,aged_70_older,gdp_per_capita,extreme_poverty,cardiovasc_death_rate,diabetes_prevalence,female_smokers,male_smokers,handwashing_facilities,hospital_beds_per_thousand,life_expectancy,human_development_index,excess_mortality_cumulative_absolute,excess_mortality_cumulative,excess_mortality,excess_mortality_cumulative_per_million)from 'owid-covid-data.csv'DELIMITER ',' CSV HEADER
 
 Drop Table if EXISTS owid_covid_data;
 CREATE TABLE  owid_covid_data(
@@ -157,16 +144,69 @@ CREATE TABLE  owid_covid_data(
 	excess_mortality_cumulative_per_million FLOAT
 );
 
+--important: copy is not a SQL command and must cp in single line to the command window
+\COPY owid_covid_data (iso_code,continent,location,d,total_cases,new_cases,new_cases_smoothed,total_deaths,new_deaths,new_deaths_smoothed,total_cases_per_million,new_cases_per_million,new_cases_smoothed_per_million,total_deaths_per_million,new_deaths_per_million,new_deaths_smoothed_per_million,reproduction_rate,icu_patients,icu_patients_per_million,hosp_patients,hosp_patients_per_million,weekly_icu_admissions,weekly_icu_admissions_per_million,weekly_hosp_admissions,weekly_hosp_admissions_per_million,new_tests,total_tests,total_tests_per_thousand,new_tests_per_thousand,new_tests_smoothed,new_tests_smoothed_per_thousand,positive_rate,tests_per_case,tests_units,total_vaccinations,people_vaccinated,people_fully_vaccinated,total_boosters,new_vaccinations,new_vaccinations_smoothed,total_vaccinations_per_hundred,people_vaccinated_per_hundred,people_fully_vaccinated_per_hundred,total_boosters_per_hundred,new_vaccinations_smoothed_per_million,stringency_index,population,population_density,median_age,aged_65_older,aged_70_older,gdp_per_capita,extreme_poverty,cardiovasc_death_rate,diabetes_prevalence,female_smokers,male_smokers,handwashing_facilities,hospital_beds_per_thousand,life_expectancy,human_development_index,excess_mortality_cumulative_absolute,excess_mortality_cumulative,excess_mortality,excess_mortality_cumulative_per_million)from 'owid-covid-data.csv'DELIMITER ',' CSV HEADER
 
 
+--Continent
+insert into Continent (continentName, countryName)
+select distinct continent as continentName, location as countryName 
+from owid_covid_data where continent is not NULL;
+
+/* --The following group data is not included and can be access as
+select distinct continent as continentName, location as countryName 
+from owid_covid_data where continent is NULL
+ORDER BY countryName;*/
+/*Some location have ISO_CODE length greater than 3, access as 
+select distinct iso_code, continent, location 
+from owid_covid_data where length(iso_code)>3
+ORDER BY LOCATION;*/
+
+--Country
+insert into Country(iso_code, countryName, population, gdp_per_capita)
+select distinct iso_code, 
+	location as countryName, 
+	cast(population as BIGINT),
+	gdp_per_capita
+	from owid_covid_data where continent is not NULL;
+
+--CoronaData
+insert into CoronaData(
+	iso_code, d, total_cases, total_deaths, reproduction_rate, total_tests,
+	total_vaccinations,people_vaccinated, people_fully_vaccinated, stringency_index)
+select 
+	iso_code, 
+	d, 
+	cast(total_cases as BIGINT), 
+	cast(total_deaths as BIGINT), 
+	reproduction_rate, 
+	cast(total_tests as BIGINT), 
+	cast(total_vaccinations as BIGINT), 
+	cast(people_vaccinated as BIGINT), 
+	cast(people_fully_vaccinated as BIGINT), 
+	stringency_index
+from owid_covid_data where continent is not NULL;
 
 
-
-
-
-
-
-
-
-
-
+--MedicalInfo
+--The insertion is successful, therefore the following data were never updated by OWID
+--In another word, they're not associated with d(date)
+insert into MedicalInfo (
+	iso_code,
+	cardiovasc_death_rate,
+	diabetes_prevalence,
+	hospital_beds_per_thousand,
+	life_expectancy,
+	female_smokers,
+	male_smokers,
+	handwashing_facilities)
+Select distinct 	
+	iso_code,
+	cardiovasc_death_rate,
+	diabetes_prevalence,
+	hospital_beds_per_thousand,
+	life_expectancy,
+	female_smokers,
+	male_smokers,
+	handwashing_facilities 
+from owid_covid_data where continent is not NULL;
