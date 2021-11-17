98752a667426da7d6831a7a79d6f252f7d8de193
Li Shuotong <Li@Shuotongs-MacBook.local>
2021-11-16 19:17:31 -0500

    schema.sql: optimization

--- schema.sql
+++ schema.sql
@@ -1,4 +1,8 @@
 --Schema for CSC343h Phase 2
+--to initialize this database, use the following command in dbsrv1:
+--Note: owid-covid-data must also be present in the same directory
+--psql csc343h-<utorid> -f 'schema.sql'
+
 
 /*
 To do:
@@ -7,9 +11,10 @@ the use of NOT NULL constrain
 */
 
 
+/*------------------------Schema------------------------*/
 DROP SCHEMA IF EXISTS COVID19 CASCADE;
 CREATE SCHEMA COVID19;
-SET SEARCH_PATH TO COVID19;
+SET SEARCH_PATH TO COVID19; /*Name of searchpath*/
 	
 --Complete
 CREATE TABLE Continent (
@@ -19,10 +24,12 @@ CREATE TABLE Continent (
 );
 
 --Complete
+--BIGINT: Some continent / world data have population greater than 2.1 billion (range of int)
+--But this table only includes countries, use BIGINT is only for futureproof
 CREATE TABLE Country (
-	iso_code VARCHAR(8), 
-	countryName TEXT NOT NULL UNIQUE, --The ISO CODE is sufficient as a primary key
-	population BIGINT, --certain value in CSV (ex. World) is greater than the range of int
+	iso_code VARCHAR(8), --Some region have OWID_ prefix
+	countryName TEXT NOT NULL, --The ISO CODE is sufficient as a primary key
+	population BIGINT, 
 	gdp_per_capita FLOAT,
 	PRIMARY KEY(iso_code),
 	FOREIGN KEY (countryName) REFERENCES Continent(countryName)
@@ -30,9 +37,10 @@ CREATE TABLE Country (
 
 --Complete
 --As many data may be missing, the NOT NULL constrain is not used here
+--BIGINT: some value can easily exceed 2.1 billion
 CREATE TABLE CoronaData (
 	iso_code VARCHAR(8),
-	d TIMESTAMP, --cannot use data as this is a keyword in sql
+	d DATE, --cannot use 'date' as this is a keyword in sql
 	total_cases BIGINT,
 	total_deaths BIGINT,
 	reproduction_rate FLOAT,
@@ -46,7 +54,6 @@ CREATE TABLE CoronaData (
 );
 
 --Complete
---Variable type is checked
 CREATE TABLE MedicalInfo (
 	iso_code VARCHAR(8),
 	cardiovasc_death_rate FLOAT,
@@ -61,7 +68,6 @@ CREATE TABLE MedicalInfo (
 );
 
 --Complete
---Variable type is checked
 CREATE TABLE DemographicInfo (
 	iso_code VARCHAR(8),
 	population_density FLOAT,
@@ -73,13 +79,16 @@ CREATE TABLE DemographicInfo (
 	PRIMARY KEY (iso_code),
 	FOREIGN KEY (iso_code) REFERENCES Country(iso_code)
 );
+/*----------------------Schema END----------------------*/
+
+
 
 
 CREATE TABLE  owid_covid_data(
-	iso_code VARCHAR(10),
+	iso_code VARCHAR(8),
 	continent TEXT,
 	location TEXT,
-	d TIMESTAMP,
+	d DATE,
 	total_cases FLOAT,
 	new_cases FLOAT,
 	new_cases_smoothed FLOAT,
